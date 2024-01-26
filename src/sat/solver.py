a4400d2c44a676c3bb91218bb665e3272282128a
M4D-A <jagielski.adamm@gmail.com>
2024-01-26 10:38:59 +0100

    formatted solver.py

--- src/sat/solver.py
+++ src/sat/solver.py
@@ -1,9 +1,10 @@
 from pysat.solvers import Cadical153, Lingeling, Glucose4
 from tempfile import NamedTemporaryFile
 from subprocess import Popen, PIPE
-from .cnf import CNF, Literal
+from .cnf import CNF
 
 Solution = tuple[bool, list[int]]
+
 
 class Solver:
     external_solvers = {
@@ -17,10 +18,11 @@ class Solver:
         "lingeling": Lingeling,
         "glucose4": Glucose4
     }
-    
-    available_solvers = list(external_solvers.keys()) + list(builtin_solvers.keys())
 
-    def __init__(self, name: str, args = None):
+    available_solvers = list(external_solvers.keys()) + \
+        list(builtin_solvers.keys())
+
+    def __init__(self, name: str, args=None):
         if name not in Solver.available_solvers:
             raise ValueError(f"Solver {name} not supported")
         self.__name = name
@@ -34,7 +36,7 @@ class Solver:
         else:
             raise ValueError(f"Solver {self.__name} not supported")
         return self._make_model(solution, cnf)
- 
+
     def _solve_builtin(self, cnf: CNF) -> Solution:
         builtin_solver_class = self.builtin_solvers[self.__name]
         clauses = cnf.clauses()
@@ -46,7 +48,7 @@ class Solver:
             return (False, [])
 
     def _solve_external(self, cnf: CNF) -> Solution:
-        args = self.external_solvers[self.__name] 
+        args = self.external_solvers[self.__name]
         if self.__args is not None:
             args += self.__args
         with NamedTemporaryFile(dir=".") as f:
@@ -72,7 +74,6 @@ class Solver:
 
     @staticmethod
     def _extract_ints(string: str) -> list[int]:
-        ints = [int(s) for s in string.split() if s.isdigit() or (s[0] == '-' and s[1:].isdigit())]
-        return [i for i in ints if i != 0]   
-
-        
+        ints = [int(s) for s in string.split() if s.isdigit()
+                or (s[0] == '-' and s[1:].isdigit())]
+        return [i for i in ints if i != 0]
