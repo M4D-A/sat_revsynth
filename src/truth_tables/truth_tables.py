fc70bde03e014a6cc6034b0a77852bcb2d745be0
M4D-A <jagielski.adamm@gmail.com>
2024-01-26 12:56:33 +0100

    Added TruthTable class implementation

---
+++ src/truth_tables/truth_tables.py
@@ -0,0 +1,57 @@
+from collections.abc import Sequence, Iterable
+
+TT = list[list[int]]
+
+
+class TruthTable:
+    def __init__(self, bits_num: int, values: Sequence[int] | None = None):
+        if values is None:
+            values = range(pow(2, bits_num))
+        else:
+            assert len(values) == pow(2, bits_num)
+
+        self._bits_num = bits_num
+        self._values = list(values)
+        self._bits = [[(i >> s) & 1 for s in range(bits_num)] for i in values]
+
+    def __len__(self):
+        return len(self._values)
+
+    def __add__(self, other):
+        assert len(self) == len(other)
+        new_values = [other.values()[v] for v in self.values()]
+        return TruthTable(self._bits_num, new_values)
+
+    def values(self):
+        return self._values
+
+    def nott(self, target: int, inplace: bool = True) -> "TruthTable":
+        if inplace:
+            for row in self._bits:
+                row[target] = 1 - row[target]
+            return self
+        else:
+            new_tt = TruthTable(self._bits_num)
+            return new_tt.nott(target, True)
+
+    def cnot(self, control: int, target: int,
+             inplace: bool = True) -> "TruthTable":
+        if inplace:
+            for row in self._bits:
+                if row[control] == 1:
+                    row[target] = 1 - row[target]
+            return self
+        else:
+            new_tt = TruthTable(self._bits_num)
+            return new_tt.cnot(control, target, True)
+
+    def mcnot(self, controls: Iterable[int], target: int,
+              inplace: bool = True) -> "TruthTable":
+        if inplace:
+            for row in self._bits:
+                if all([row[control] == 1 for control in controls]):
+                    row[target] = 1 - row[target]
+            return self
+        else:
+            new_tt = TruthTable(self._bits_num)
+            return new_tt.mcnot(controls, target, True)
