2888d86535226f249193891bc91bfb230cf9ba30
M4D-A <jagielski.adamm@gmail.com>
2024-01-29 12:34:58 +0100

    Transfer commit

--- src/circuit/circuit_test.py
+++ src/circuit/circuit_test.py
@@ -1,6 +1,7 @@
 import pytest
 from random import randint, sample
-from .circuit import Circuit
+from .circuit import Circuit, Gate
+from truth_tables import TruthTable
 from copy import deepcopy
 
 
@@ -56,3 +57,67 @@ def test_mcx(circ_size):
                 assert ref_b != b
             else:
                 assert ref_b == b
+
+
+@pytest.mark.parametrize("circ_size", size_randomizer)
+def test_x_involutivity(circ_size):
+    circ = Circuit(circ_size)
+    target = randint(0, circ_size - 1)
+    circ.x(target)
+    circ.x(target)
+    assert len(circ) == 2
+    assert all(gate == ([], target) for gate in circ.gates())
+    assert circ.tt() == TruthTable(circ_size)
+
+
+@pytest.mark.parametrize("circ_size", size_randomizer)
+def test_cx_involutivity(circ_size):
+    circ = Circuit(circ_size)
+    control, target = sample(range(0, circ_size - 1), 2)
+    circ.cx(control, target)
+    circ.cx(control, target)
+    assert len(circ) == 2
+    assert all(gate == ([control], target) for gate in circ.gates())
+    assert circ.tt() == TruthTable(circ_size)
+
+
+@pytest.mark.parametrize("circ_size", size_randomizer)
+def test_mcx_involutivity(circ_size):
+    circ = Circuit(circ_size)
+    special_ids_num = randint(2, circ_size - 1)
+    target, *controls = sample(range(0, circ_size - 1), special_ids_num)
+    circ.mcx(controls, target)
+    circ.mcx(controls, target)
+    assert len(circ) == 2
+    assert all(gate == (sorted(controls), target) for gate in circ.gates())
+    assert circ.tt() == TruthTable(circ_size)
+
+
+@pytest.mark.parametrize("circ_size", size_randomizer)
+def test_complex_involution(circ_size):
+    gates: list[Gate] = []
+    gates_num = randint(2, 8)
+    for _ in range(gates_num):
+        controls_num = randint(0, circ_size - 2)
+        ids = sample(range(0, circ_size - 1), controls_num + 1)
+        target = ids[0]
+        controls = [] if len(ids) == 1 else ids[0:]
+        gates.append(Gate((list(controls), target)))
+
+    circ = Circuit(circ_size)
+    for gate in gates:
+        circ.append(gate)
+
+    for gate in deepcopy(gates)[::-1]:
+        circ.append(gate)
+
+    assert len(circ) == 2 * gates_num
+
+    for i, g in enumerate(circ.gates()):
+        if i < gates_num:
+            controls, target = gates[i]
+        else:
+            controls, target = gates[len(circ) - i - 1]
+        assert g == (sorted(controls), target)
+
+    assert circ.tt() == TruthTable(circ_size)
