accf0292aad5f9b64b0bf079b4179b584881c35e
M4D-A <jagielski.adamm@gmail.com>
2024-01-29 15:00:54 +0100

    Adjusted TT class member functions to fit those in Circuit

--- src/truth_table/truth_table_test.py
+++ src/truth_table/truth_table_test.py
@@ -11,11 +11,11 @@ size_randomizer = list(randint(3, max_tt_size) for _ in range(epochs))
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_not(tt_size):
+def test_x(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     target = randint(0, tt.bits_num() - 1)
-    tt.nott(target)
+    tt.x(target)
     for ref_row, row in zip(ref_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target:
@@ -25,11 +25,11 @@ def test_not(tt_size):
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_cnot(tt_size):
+def test_cx(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     control, target = sample(range(0, tt.bits_num() - 1), 2)
-    tt.cnot(control, target)
+    tt.cx(control, target)
     for ref_row, row in zip(ref_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target and row[control] == 1:
@@ -39,12 +39,12 @@ def test_cnot(tt_size):
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_mcnot(tt_size):
+def test_mcx(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     special_ids_num = randint(2, tt.bits_num() - 1)
     target, *controls = sample(range(0, tt.bits_num() - 1), special_ids_num)
-    tt.mcnot(controls, target)
+    tt.mcx(controls, target)
     for ref_row, row in zip(ref_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target and all(row[c] for c in controls):
@@ -54,31 +54,31 @@ def test_mcnot(tt_size):
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_not_involutivity(tt_size):
+def test_x_involutivity(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     target = randint(0, tt.bits_num() - 1)
-    tt.nott(target)
-    tt.nott(target)
+    tt.x(target)
+    tt.x(target)
     assert tt == ref_tt
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_cnot_involutivity(tt_size):
+def test_cx_involutivity(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     control, target = sample(range(0, tt.bits_num() - 1), 2)
-    tt.cnot(control, target)
-    tt.cnot(control, target)
+    tt.cx(control, target)
+    tt.cx(control, target)
     assert tt == ref_tt
 
 
 @pytest.mark.parametrize("tt_size", size_randomizer)
-def test_mcnot_involutivity(tt_size):
+def test_mcx_involutivity(tt_size):
     ref_tt = TruthTable(tt_size).shuffle()
     tt = deepcopy(ref_tt)
     special_ids_num = randint(2, tt.bits_num() - 1)
     target, *controls = sample(range(0, tt.bits_num() - 1), special_ids_num)
-    tt.mcnot(controls, target)
-    tt.mcnot(controls, target)
+    tt.mcx(controls, target)
+    tt.mcx(controls, target)
     assert tt == ref_tt
