3050713e42e9ac3029b2575dbf317d95a4385460
M4D-A <jagielski.adamm@gmail.com>
2024-01-29 22:24:10 +0100

    upgraded TruthTable UT fixtures

--- src/truth_table/truth_table_test.py
+++ src/truth_table/truth_table_test.py
@@ -1,8 +1,7 @@
 import pytest
-from random import randint
+from random import randint, sample
 from copy import copy
 from .truth_table import TruthTable
-from ..utils.params import x_params, cx_params, mcx_params
 
 
 max_bits_size = 8
@@ -10,14 +9,41 @@ epochs = 2**8
 bits_num_randomizer = list(randint(3, max_bits_size) for _ in range(epochs))
 
 
-@pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_x(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
+@pytest.fixture
+def x_params(bits_num):
+    target = randint(0, bits_num - 1)
+    return target
 
-    target = x_params(bits_num)
+
+@pytest.fixture
+def cx_params(bits_num):
+    control, target = sample(range(0, bits_num - 1), 2)
+    return control, target
+
+
+@pytest.fixture
+def mcx_params(bits_num):
+    special_ids_num = randint(2, bits_num - 1)
+    target, *controls = sample(range(0, bits_num - 1), special_ids_num)
+    return controls, target
+
+
+@pytest.fixture
+def random_tt(bits_num):
+    return TruthTable(bits_num).shuffle()
+
+
+@pytest.fixture
+def identity_tt(bits_num):
+    return TruthTable(bits_num)
+
+
+@pytest.mark.parametrize("bits_num", bits_num_randomizer)
+def test_x(x_params, random_tt):
+    tt = copy(random_tt)
+    target = x_params
     tt.x(target)
-    for ref_row, row in zip(ref_tt.bits(), tt.bits()):
+    for ref_row, row in zip(random_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target:
                 assert ref_b != b
@@ -26,12 +52,11 @@ def test_x(bits_num):
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_cx(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
-    control, target = cx_params(bits_num)
+def test_cx(cx_params, random_tt):
+    tt = copy(random_tt)
+    control, target = cx_params
     tt.cx(control, target)
-    for ref_row, row in zip(ref_tt.bits(), tt.bits()):
+    for ref_row, row in zip(random_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target and row[control] == 1:
                 assert ref_b != b
@@ -40,12 +65,11 @@ def test_cx(bits_num):
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_mcx(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
-    controls, target = mcx_params(bits_num)
+def test_mcx(mcx_params, random_tt):
+    tt = copy(random_tt)
+    controls, target = mcx_params
     tt.mcx(controls, target)
-    for ref_row, row in zip(ref_tt.bits(), tt.bits()):
+    for ref_row, row in zip(random_tt.bits(), tt.bits()):
         for i, (ref_b, b) in enumerate(zip(ref_row, row)):
             if i == target and all(row[c] for c in controls):
                 assert ref_b != b
@@ -54,51 +78,48 @@ def test_mcx(bits_num):
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_x_involutivity(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
-    target = x_params(bits_num)
+def test_x_involutivity(x_params, random_tt):
+    tt = copy(random_tt)
+    target = x_params
     tt.x(target)
     tt.x(target)
-    assert tt == ref_tt
+    assert tt == random_tt
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_cx_involutivity(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
-    control, target = cx_params(bits_num)
+def test_cx_involutivity(cx_params, random_tt):
+    tt = copy(random_tt)
+    control, target = cx_params
     tt.cx(control, target)
     tt.cx(control, target)
-    assert tt == ref_tt
+    assert tt == random_tt
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_mcx_involutivity(bits_num):
-    ref_tt = TruthTable(bits_num).shuffle()
-    tt = copy(ref_tt)
-    controls, target = mcx_params(bits_num)
+def test_mcx_involutivity(mcx_params, random_tt):
+    tt = copy(random_tt)
+    controls, target = mcx_params
     tt.mcx(controls, target)
     tt.mcx(controls, target)
-    assert tt == ref_tt
+    assert tt == random_tt
 
 
 @pytest.mark.parametrize("bits_num", bits_num_randomizer)
-def test_inplace(bits_num):
-    tt_a = TruthTable(bits_num)
-    target = x_params(bits_num)
+def test_inplace(x_params, identity_tt):
+    tt_a = copy(identity_tt)
+    target = x_params
 
     tt_b = tt_a.x(target, inplace=False)
-    assert tt_a == TruthTable(bits_num)
-    assert tt_b != TruthTable(bits_num)
+    assert tt_a == identity_tt
+    assert tt_b != identity_tt
 
     tt_a.x(target, inplace=True)
-    assert tt_a != TruthTable(bits_num)
-    assert tt_b != TruthTable(bits_num)
+    assert tt_a != identity_tt
+    assert tt_b != identity_tt
     assert tt_b == tt_a
 
     tt_a.x(target)
     tt_b.x(target)
-    assert tt_a == TruthTable(bits_num)
-    assert tt_b == TruthTable(bits_num)
+    assert tt_a == identity_tt
+    assert tt_b == identity_tt
     assert tt_b == tt_a
