321681182e11a1dd7ed59c5575a17e31552d176e
M4D-A <jagielski.adamm@gmail.com>
2024-02-07 23:03:16 +0100

    added synthesizer, has some bugs

--- src/main.py
+++ src/main.py
@@ -2,5 +2,11 @@ from truth_table.truth_table import TruthTable
 from sat.solver import Solver
 from sat.cnf import CNF
 from circuit.circuit import Circuit
-t = TruthTable(3)
+from synthesizer.synthesizer import Synthesiser
+
+t = TruthTable(2, [2, 1, 3, 0])
+s = Solver("kissat")
+synth = Synthesiser(t, 3, s)
+c = synth.solve()
 print(t)
+print(c)
--- src/synthesizer/synthesizer.py
+++ src/synthesizer/synthesizer.py
@@ -33,26 +33,29 @@ class Synthesiser:
         self._width = len(output[0])
         self._words = len(output)
         # self.ancilla_num = ancilla_num
-        self.max_clauses_len = 3
-        self.encoding = 1
+        self._max_clauses_len = 3
+        self._encoding = 1
         # self.solution = None
         # self.circuit = None
+        self._cnf, self._controls, self._targets = self._make_revcirc_cnf()
 
     def _make_revcirc_cnf(self):
         cnf = CNF()
         width_iter = range(self._width)
         gc_iter = range(self._gate_count)
         input_iter = range(self._words)
+        print(f"{self._width = } {self._gate_count = } {self._words = }")
 
         # i - qubit index, j - layer index, w - input word index
         controls = [[cnf.reserve_name(f"c_{i}_{j}") for i in width_iter] for j in gc_iter]
         targets = [[cnf.reserve_name(f"t_{i}_{j}") for i in width_iter] for j in gc_iter]
+        print(targets)
 
         data_bits = [[[cnf.reserve_name(f"d_{i}_{j}_{w}") for i in width_iter]
-                      for j in gc_iter] for w in input_iter]
+                      for j in range(self._gate_count+1)] for w in input_iter]
         switch_bits = [[[cnf.reserve_name(f"s_{i}_{j}_{w}") for i in width_iter]
                         for j in gc_iter] for w in input_iter]
-        add_bits = [[cnf.reserve_name(f"a_{j}_{d}") for j in gc_iter] for d in input_iter]
+        add_bits = [[cnf.reserve_name(f"a_{j}_{w}") for j in gc_iter] for w in input_iter]
         or_bits = [[[cnf.reserve_name(f"o_{i}_{j}_{w}") for i in width_iter]
                     for j in gc_iter] for w in input_iter]
 
@@ -63,32 +66,54 @@ class Synthesiser:
 
         # Target qubit cannot be a control qubit
         for i, j in product(width_iter, gc_iter):
-            cnf.nand(targets[i][j], controls[i][j])
+            print(f"{i=} {j=}")
+            cnf.nand(targets[j][i], controls[j][i])
 
         # Data flow constraints
         # Target qubit is the data bit
         for i, j, w in product(width_iter, gc_iter, input_iter):
-            cnf.equals_or(or_bits[i][j][w], [data_bits[i][j][w], -controls[i][j]])
+            cnf.equals_or(or_bits[w][j][i], [data_bits[w][j][i], -controls[j][i]])
 
         # Add bit is the or of all or bits
         for j, w in product(gc_iter, input_iter):
-            l_list = [or_bits[i][j][w] for i in width_iter]
-            cnf.equals_and(add_bits[j][w], l_list)
+            l_list = [or_bits[w][j][i] for i in width_iter]
+            cnf.equals_and(add_bits[w][j], l_list)
 
         # Switch bit is the add bit and the target qubit
         for i, j, w in product(width_iter, gc_iter, input_iter):
-            cnf.equals_and(switch_bits[i][j][w], [add_bits[j][w], targets[i][j]])
+            cnf.equals_and(switch_bits[w][j][i], [add_bits[w][j], targets[j][i]])
 
         # Data bit is the previous data bit xored with the switch bit
-        for i, j, w in product(width_iter, gc_iter, input_iter):
-            cnf.xor([data_bits[i][j+1][w], data_bits[i][j][w], switch_bits[i][j][w]])
+        for i, j, w in product(width_iter, range(self._gate_count - 1), input_iter):
+            cnf.xor([data_bits[w][j+1][i], data_bits[w][j][i], switch_bits[w][j][i]])
 
         # Input/Output edge constraints
         for i, w in product(width_iter, input_iter):
-            cnf.set_literal(data_bits[i][0][w], (w >> i & 1 == 1))
+            cnf.set_literal(data_bits[w][0][i], (w >> i & 1 == 1))
 
         for i, w in product(width_iter, input_iter):
             if self._output[w][i] in [0, 1]:
-                cnf.set_literal(data_bits[i][self._gate_count][w], (self._output[w][i] == 1))
+                a = data_bits[w][self._gate_count][i]
+                b = (self._output[w][i] == 1)
+                cnf.set_literal(a, b)
 
         return cnf, controls, targets
+
+    def solve(self):
+        width_iter = range(self._width)
+        gc_iter = range(self._gate_count)
+        self.solution = self._solver.solve(self._cnf)
+        if not self.solution["sat"]:
+            self.circuit = None
+            return self.circuit
+        circuit = Circuit(self._width)
+        print(self.solution)
+        for j in gc_iter:
+            targets = [i for i in width_iter if self.solution[f"t_{i}_{j}"]]
+            controls = [i for i in width_iter if self.solution[f"c_{i}_{j}"]]
+            print(targets, controls)
+            assert len(targets) == 1
+            circuit.append((controls, targets[0]))
+        self._circuit = circuit
+        print(self._circuit.tt())
+        return self._circuit
