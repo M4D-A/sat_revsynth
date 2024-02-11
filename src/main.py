ff04717aac3ed266a74ab73bb096814e190123fa
M4D-A <jagielski.adamm@gmail.com>
2024-02-11 20:29:17 +0100

    Added simple test cases for synthesiser

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from sat.solver import Solver
 from synthesizer.synthesizer import Synthesizer
 tt = TruthTable(2)
 s = Solver("kissat")
-sy = Synthesizer(tt, 3, s)
+sy = Synthesizer(tt, 4, s)
 c = sy.solve()
-print(c is None)
+print(c)
