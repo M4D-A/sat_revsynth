d058ab203bf51fd00a0ad31608981c21788bdb9e
M4D-A <jagielski.adamm@gmail.com>
2024-02-18 17:39:11 +0100

    removed unused imports

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from collection_synthesizer.collection_synthesizer import CollectionSynthesizer
-from dimgroup_synthesizer.dimgroup_synthesizer import DimGroupSynthesizer
 
 cs = CollectionSynthesizer(3, 5)
 collection = cs.synthesize(16)
