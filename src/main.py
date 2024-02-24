3a8589ac63d8c1462c06ab3ae25a3753a9f8f873
M4D-A <jagielski.adamm@gmail.com>
2024-02-24 05:21:45 +0100

    Distiller finished

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@ from synthesizers.collection_synthesizer import CollectionSynthesizer
 from excirc_distiller.excirc_distiller import ExCircDistiller
 from pickle import load
 
-cs = CollectionSynthesizer(3, 7)
-cs.set_file_save("/home/adam/data", "test_1")
-collection = cs.synthesize(16)
+# cs = CollectionSynthesizer(3, 7)
+# cs.set_file_save("/home/adam/data", "test_1")
+# collection = cs.synthesize(16)
 
-# with open("/home/adam/data/test_1_3_7.pickle", "rb") as f:
-#     collection = load(f)
+with open("/home/adam/data/test_1_3_7.pickle", "rb") as f:
+    collection = load(f)
 # print(collection)
-#
-# ecd = ExCircDistiller(collection)
-# exc_collection = ecd.distill()
-# print()
-# print("----------")
-# print(exc_collection)
+
+ecd = ExCircDistiller(collection)
+exc_collection = ecd.distill()
+print()
+print("----------")
+print(exc_collection)
