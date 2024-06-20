c72e89664180b66979f44d9381c3fa366d0f09fd
M4DA <jagielski.adamm@gmail.com>
2024-06-20 16:56:31 +0200

    Sbox class and algebraic degree

--- src/main.py
+++ src/main.py
@@ -1,25 +1,21 @@
-from circuit.collection import Collection
-from truth_table.truth_table import TruthTable
-from synthesizers.optimal_synthesizer import OptimalSynthesizer
-from sat.solver import Solver
+from sbox.sbox import Sbox 
+from random import shuffle
 
-c = Collection(4, 4).from_file("/home/adam/exc_4_7.txt")
-# print(c)
 
-elephant_sbox = [
-    0x0E, 0x0D, 0x0B, 0x00, 0x02, 0x01, 0x04, 0x0F,
-    0x07, 0x0A, 0x08, 0x05, 0x09, 0x0C, 0x03, 0x06
-]
-photon_beatle_sbox = [
-    0x0C, 0x05, 0x06, 0x0B, 0x09, 0x00, 0x0A, 0x0D,
-    0x03, 0x0E, 0x0F, 0x08, 0x04, 0x07, 0x01, 0x02
-]
+bits = 4
+size = 2**bits
 
-tt = TruthTable(4, elephant_sbox)
+table = list(range(size))
 
-print(tt.values())
-os = OptimalSynthesizer(tt, 0, 11, Solver("kissat"))
-os.exclude_collection(c)
-cq = os.solve()
+counter = [0] * (bits + 1)
+for i in range(100_000):
+    shuffle(table)
+    sbox = Sbox(bits, table)
+    d1 = sbox.algebraic_degree()
+    counter[d1] += 1
+    if ((i+1) % 10_000) == 0:
+        print(counter)
 
-print(cq)
+
+print(counter)
+
